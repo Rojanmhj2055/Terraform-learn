fb1e3cad4242a3ecb06c6528fd84f0d1c1b04d1b
Rojan Maharjan <rojanmhj@gmail.com>
2023-07-15 22:08:10 -0700

    created AWS VPC and Subnets

--- main.tf
+++ main.tf
@@ -8,5 +8,33 @@ terraform {
 }
 
 provider "aws" {
- 
+}
+
+variable "env_prefix"{
+
+}
+
+variable "vpc_cidr_block" {
+  
+}
+variable "subnet_cidr_blocks" {
+}
+
+variable "avail_zone" {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    "Name" = "${var.env_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "my-app-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_blocks
+  availability_zone = var.avail_zone
+  tags = {
+    "Name" = "${var.env_prefix}-subnet-1"
+  }
+
 }
