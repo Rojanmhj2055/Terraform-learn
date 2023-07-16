aef77775f9902f239ce65d95ce005c313938cd33
Rojan Maharjan <rojanmhj@gmail.com>
2023-07-15 22:16:18 -0700

    Route Table and Internet Gateway

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ variable "avail_zone" {}
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    "Name" = "${var.env_prefix}-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
@@ -34,7 +34,26 @@ resource "aws_subnet" "my-app-subnet-1" {
   cidr_block = var.subnet_cidr_blocks
   availability_zone = var.avail_zone
   tags = {
-    "Name" = "${var.env_prefix}-subnet-1"
+    Name = "${var.env_prefix}-subnet-1"
   }
 
+}
+
+## Route Table and Internet Gateway
+resource "aws_internet_gateway" "my-app-gw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name ="${var.env_prefix}-gw" 
+  }
+}
+
+resource "aws_route_table" "my-app-route" {
+  vpc_id=aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-app-gw.id
+  }
+  tags={
+    Name="${var.env_prefix}-rtb"
+  }
 }
