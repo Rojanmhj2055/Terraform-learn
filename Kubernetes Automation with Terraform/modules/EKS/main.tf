e99763b61d658d80817c369cf9c4f72e008da070
Rojan Maharjan <rojanmhj@gmail.com>
2023-07-26 22:26:35 -0700

    Completed EKS Cluster

--- Kubernetes Automation with Terraform/modules/EKS/main.tf
+++ Kubernetes Automation with Terraform/modules/EKS/main.tf
@@ -69,7 +69,7 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"'
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role = aws_iam_role.nodes.name
 }
 
@@ -103,3 +103,70 @@ resource "aws_eks_node_group" "private-nodes"{
     aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
   ]
   }
+  
+  #Create IAM OIDC provider EKS using Terraform
+#To manage permissions for your applications that you deploy in Kubernetes. You can either attach
+# policies to Kubernetes nodes directly. In that case, every pod will get the same access to AWS 
+#resources. Or you can create OpenID connect provider, which will allow granting IAM permissions 
+#based on the service account used by the pod.
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+}
+
+#TEST
+
+
+data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:default:aws-test"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "test_oidc" {
+  assume_role_policy = data.aws_iam_policy_document.test_oidc_assume_role_policy.json
+  name               = "test-oidc"
+}
+
+resource "aws_iam_policy" "test-policy" {
+  name = "test-policy"
+
+  policy = jsonencode({
+    Statement = [{
+      Action = [
+        "s3:ListAllMyBuckets",
+        "s3:GetBucketLocation"
+      ]
+      Effect   = "Allow"
+      Resource = "arn:aws:s3:::*"
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "test_attach" {
+  role       = aws_iam_role.test_oidc.name
+  policy_arn = aws_iam_policy.test-policy.arn
+}
+
+output "test_policy_arn" {
+  value = aws_iam_role.test_oidc.arn
+}
